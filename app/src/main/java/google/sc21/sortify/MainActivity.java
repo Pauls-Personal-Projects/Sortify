50682f5b02c3ca63e5a98da0f9240219a069149e
Paul Johannes Aru <pauljohannes.aru@gmail.com>
2021-03-12 14:16:56 +0000

    First Commit! Empty Android Studio Template.

---
+++ app/src/main/java/google/sc21/sortify/MainActivity.java
@@ -0,0 +1,14 @@
+package google.sc21.sortify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
