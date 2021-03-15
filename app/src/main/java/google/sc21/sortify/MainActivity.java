728a66ed7cb5513593c65a10e17c823ec694e746
Paul Johannes Aru <pauljohannes.aru@gmail.com>
2021-03-15 12:52:32 +0000

    Camera & Image Capture Working. Might need to investigate the image resolution? Also GUI Looks a bit wonky, might need to work on that as well if we have time.

--- app/src/main/java/google/sc21/sortify/MainActivity.java
+++ app/src/main/java/google/sc21/sortify/MainActivity.java
@@ -6,16 +6,53 @@ package google.sc21.sortify;
 
 // IMPORTS
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 // MAIN
 public class MainActivity extends AppCompatActivity {
+    // GUI Objects:
     Button cameraButton;
     TextView testLabel;
+    ImageView imageBox;
+    // Constant for our Image Capture:
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
+
+
+    // Calls Camera Activity:
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            Log.e(String.valueOf(e), "Camera Intent not Found: ");
+            testLabel.setText("Houston, we have a problem!?");
+        }
+    }
+    // Returns Image from Camera Activity:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBox.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+    //MAIN:
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Connect GUI with Code
@@ -23,10 +60,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         cameraButton = findViewById(R.id.cameraButton);
         testLabel = findViewById(R.id.helloLabel);
+        imageBox = findViewById(R.id.photoView);
 
         // Camera Button Pressed
         cameraButton.setOnClickListener(v -> {
             testLabel.setText("Open Camera");
+            dispatchTakePictureIntent();
         });
     }
 }
